b0b07ed079cef90f19314cd69b5c8875d71202d4
Isabel Sáenz <isasaete@gmail.comn>
2025-02-09 11:29:12 +0100

    Second correction of variable names

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const username = "isasaete";
-const minimunCharactersUsername = username.length >= 5;
-const isValidUsernameLength = minimunCharactersUsername;
+const usernameMinimumLength = 5;
+const isValidUsername = username.length >= usernameMinimumLength;
 
 const password = "ProgrÁmÀ2";
-const minimunCharactersPassword = 8;
-const isValidPasswordLength = password.length >= minimunCharactersPassword;
+const passwordMinimumLength = 8;
+const isValidPasswordLength = password.length >= passwordMinimumLength;
 const hasPasswordNumber = /\d/.test(password);
 const hasPasswordUppercase = /[A-ZÁÉÍÓÚÑáéíóúñÀÈÌÒÙàèìòù]/.test(password);
 const isValidPassword =
@@ -17,7 +17,7 @@ const age = 34;
 const minimumAdultAge = 18;
 const isAdult = age >= minimumAdultAge;
 
-console.log(`El nombre de usuario es válido: ${isValidUsernameLength}`);
+console.log(`El nombre de usuario es válido: ${isValidUsername}`);
 console.log(`La contraseña es válida: ${isValidPassword} `);
 console.log(`La repetición de la contraseña es válida: ${isPasswordRepeat}`);
 console.log(`Es mayor de edad: ${isAdult}`);
